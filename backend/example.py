4e962b424271f104e523c4ca5299b324b8886edf
martindoychinov07 <114239959+martindoychinov07@users.noreply.github.com>
2023-04-04 20:44:14 +0300

    Add files via upload

--- backend/example.py
+++ backend/example.py
@@ -1,4 +1,5 @@
 from flask import Flask,request, session, jsonify
+from flask_cors import CORS
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 import hashlib
@@ -8,6 +9,7 @@ from notification import *
 
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = "Python"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
